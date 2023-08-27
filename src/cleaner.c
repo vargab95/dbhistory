ff4e140a65b2f41932c25a7a466e320f89999d51
Balázs János Varga <vargab95@gmail.com>
2023-08-27 21:02:24 +0200

    Use SQL to delete old history records

--- src/cleaner.c
+++ src/cleaner.c
@@ -13,7 +13,6 @@
 
 static void check_non_referenced_path(const char *path);
 static void check_non_existing_path(const char *path);
-static int delete_old_records_callback(void *data, int argc, char **argv, char **col_names);
 static void delete_old_records();
 
 extern void cleaner_run()
@@ -27,6 +26,8 @@ extern void cleaner_run()
         return;
     }
 
+    delete_old_records();
+
     if (DB_SUCCESS != db_get_pathes(&pathes, &path_count))
     {
         print_message(MSG_ERROR, "Cannot read pathes!\n");
@@ -38,8 +39,6 @@ extern void cleaner_run()
         check_non_referenced_path(pathes[i]);
         check_non_existing_path(pathes[i]);
     }
-
-    delete_old_records();
 
     db_close();
 }
@@ -86,7 +85,7 @@ static void check_non_existing_path(const char *path)
 
 static void delete_old_records()
 {
-    const char get_records_cmd[] = "SELECT id, timestamp FROM history;";
+    const char delete_records_cmd[] = "DELETE FROM history WHERE timestamp < \"%s\";";
 
     if (g_dbhistory_configuration.deletion_time_threshold < 0)
     {
@@ -94,48 +93,16 @@ static void delete_old_records()
         return;
     }
 
-    if (DB_SUCCESS != sql_run_command(delete_old_records_callback, NULL, get_records_cmd))
+    time_t now = time(0);
+    const char buffer[128];
+    time_t deletion_threshold = now - g_dbhistory_configuration.deletion_time_threshold;
+    struct tm *deletion_threshold_tm = localtime(&deletion_threshold);
+
+    strftime((char*)buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", deletion_threshold_tm);
+
+    if (DB_SUCCESS != sql_run_command(NULL, NULL, delete_records_cmd, buffer))
     {
-        print_message(MSG_ERROR, "Selecting all records for check was failed.\n");
+        print_message(MSG_ERROR, "Deleting all old records was failed.\n");
         return;
     }
-}
-
-static int delete_old_records_callback(void *data, int argc, char **argv, char **col_names)
-{
-    struct tm tm;
-    int record_id;
-    time_t record_timestamp;
-    time_t current_time;
-
-    print_message(MSG_TRACE, "Delete old records was called.\n");
-    print_message(MSG_TRACE, "argv[0]: %s, argv[1]: %s\n", argv[0], argv[1]);
-
-    record_id = atoi(argv[0]);
-
-    print_message(MSG_TRACE, "Try to decode timestamp\n");
-    strptime(argv[1], "%Y-%m-%d %H:%M:%S", &tm);
-    record_timestamp = mktime(&tm);
-
-    print_message(MSG_TRACE, "Reading current time\n");
-    time(&current_time);
-
-    print_message(MSG_TRACE, "Testing timestamp\n");
-    print_message(MSG_DEBUG, "Current time %d, threshold %d, record timestamp %d\n", current_time,
-                  g_dbhistory_configuration.deletion_time_threshold, record_timestamp);
-    if ((current_time - g_dbhistory_configuration.deletion_time_threshold) >= record_timestamp)
-    {
-        print_message(MSG_WARNING, "Record %d is out of date\n", record_id);
-        if (DB_SUCCESS !=
-            sql_run_command(delete_old_records_callback, NULL, "DELETE FROM history WHERE id=%d;", record_id))
-        {
-            print_message(MSG_ERROR, "Couldn't delete record %d!\n", record_id);
-            return 0;
-        }
-
-        print_message(MSG_INFO, "Record %d deleted successfully.\n", record_id);
-        return 0;
-    }
-
-    return 0;
 }
